44766e17a8a6edbd45fa8877f4960acd35517d49
Your Name <you@example.com>
2022-07-05 10:05:05 +0700

    stopWatch

---
+++ demo.js
@@ -0,0 +1,19 @@
+class StopWatch {
+    startTime;
+    endTime;
+    constructor(starTime,endTime,milisecond) {
+        this.startTime=starTime;
+        this.endTime=endTime;
+
+    }
+    getstarTime=()=>this.startTime;
+    getendTime=()=>this.endTime;
+    start=()=>this.startTime= new Date();
+    stop=()=>this.endTime= new Date();
+    getElapsed=()=>this.endTime-this.startTime;
+}
+
+let time=new StopWatch();
+console.log(time.start());
+console.log(time.stop());
+console.log(time.getElapsed());
